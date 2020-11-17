539e3f281b9fef11eb0fde9be91102b1d6aa8b12
Ferdinand737 <ferdinandhaaben@gmail.com>
2020-11-16 16:11:55 -0800

    Worked more on milestone2.ipynb

--- analysis/Scripts/project_functions.py
+++ analysis/Scripts/project_functions.py
@@ -4,9 +4,7 @@ import numpy as np
 
 def load_and_proccess(rawData):
     rawData.columns = ['Age','Workclass','fnlwgt','Education','Education-Num','Marital-Status','Occupation','Realtionship','Race','Sex','Capital-Gain','Capital-Loss','Hours-Per-Week','Native-Country','Income']
-    df1 = (rawData.drop(columns = ['fnlwgt','Education-Num',]))
-    
-    return df1
+    return rawData
 
 def describedf(data):
     return data.describe()
